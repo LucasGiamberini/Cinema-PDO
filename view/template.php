79566acf164cace3f292dbf4159b850b3fd22095
LucasGiamberini <lgiamberini1@gmail.com>
2024-05-05 10:13:35 +0200

    adding some change

--- view/template.php
+++ view/template.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="public/css/style.css">
     <script src="public\script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
 
